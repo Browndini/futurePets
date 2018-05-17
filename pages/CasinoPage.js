dcc5455a5d96ca934a7340ed711b8630d5aaf252
Browndini <dt.kylebrown@gmail.com>
2018-05-16 19:58:10 -0700

    working on the casino slots. Testing react native game engine.

--- pages/CasinoPage.js
+++ pages/CasinoPage.js
@@ -1,58 +1,49 @@
-import React, { Component } from "react";
-import { bindActionCreators } from "redux";
-import { connect } from "react-redux";
+import React, { PureComponent } from "react";
 import {
-  Animated,
+  AppRegistry,
   StyleSheet,
-  Text,
+  StatusBar,
   View,
   Image,
   TouchableOpacity,
-  ScrollView
+  Dimensions
 } from "react-native";
-import _ from "lodash";
-// import SquareRow from "../components/square/SquareRow";
-import Slots from "../components/casino/Slots";
-import TimerMixin from "react-timer-mixin";
-// import MapPage from "./MapPage";
-import * as Actions from "../actions/actions";
+import { GameEngine, GameLoop } from "react-native-game-engine";
+import { Finger } from "../components/casino/Finger";
+import { MoveFinger } from "../components/casino/MoveFinger";
+import { Wall } from "../components/casino/Wall";
 
-class CasinoPage extends Component {
-  state = {
-    fadeAnim: new Animated.Value(0),
-    spinnerOne: new Animated.Value(0),
-    spinnerTwo: new Animated.Value(0),
-    spinnerThree: new Animated.Value(0),
-    mixins: [TimerMixin],
-    stop: false
-  };
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
+const RADIUS = 25;
+const SQUARE = 50;
 
-  componentDidMount() {
-    Animated.timing(this.state.fadeAnim, {
-      toValue: 1,
-      duration: 1000,
-      useNativeDriver: true
-    }).start();
+export default class BestGameEver extends PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      x: WIDTH / 2 - RADIUS,
+      y: HEIGHT / 2 - RADIUS,
+      squareX: 230,
+      squareY: 100,
+      squareStart: -100
+    };
   }
+  // updateHandler = ({ touches, screen, time }) => {
+  //   touches.find(x => console.log(x));
+  //   let move = touches.find(x => x.type === "move");
+  //   if (move) {
+  //     this.setState({
+  //       x: this.state.x + move.delta.pageX,
+  //       y: this.state.y + move.delta.pageY,
+  //       squareX: this.state.squareX + move.delta.pageX,
+  //       squareY: this.state.squareY + move.delta.pageY
+  //     });
+  //   }
+  // };
 
   render() {
-    const {
-      color,
-      user: {
-        credits: { checking, savings }
-      },
-      pets
-    } = this.props;
-    // console.log(this.props);
-    const { fadeAnim } = this.state;
-
     return (
-      <Animated.View
-        style={[
-          styles.container
-          // { opacity: fadeAnim }
-        ]}
-      >
+      <View>
         <TouchableOpacity
           style={{ position: "absolute", top: 25, left: 0 }}
           onPressOut={() => this.props.navigator.pop()}
@@ -63,70 +54,48 @@ class CasinoPage extends Component {
             source={{ uri: "/Users/kylebrown/futurePets/images/back.png" }}
           />
         </TouchableOpacity>
-        <ScrollView style={{ flex: 1, marginTop: 55 }}>
-          <Slots />
-          <TouchableOpacity
-            style={{
-              flex: 1,
-              marginTop: 20,
-              backgroundColor: "#FF9B42",
-              alignSelf: "center",
-              borderRadius: 10,
-              paddingTop: 8,
-              paddingBottom: 8,
-              paddingLeft: 20,
-              paddingRight: 20
-            }}
-            onPress={() => {
-              this.setState({ stop: true });
-            }}
-          >
-            <Text style={{ fontSize: 24 }}>STOP</Text>
-          </TouchableOpacity>
-        </ScrollView>
-      </Animated.View>
+        <GameEngine
+          style={styles.container}
+          systems={[MoveFinger]}
+          entities={{
+            1: { position: [40, 200], renderer: <Finger /> }, //-- Notice that each entity has a unique id (required)
+            2: {
+              square: [this.state.squareX, this.state.squareY],
+              position: [this.state.squareX, this.state.squareY],
+              renderer: <Wall />
+            }
+          }}
+          // renderer={(entities, screen) => {
+          //   console.log(entities);
+          //   return screen;
+          // }}
+        >
+          <View style={styles.player} />
+          <StatusBar hidden={true} />
+        </GameEngine>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  nameText: { fontSize: 14, fontWeight: "800" },
-  images: { height: 100, width: 100, margin: 5 },
-  singleBox: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#DEDBD2",
-    margin: 5,
-    minWidth: 100
-  },
-  headerText: {
-    fontSize: 40,
-    textAlign: "center"
-  },
-  header: {
-    flex: 0.3,
-    marginTop: 20,
-    flexDirection: "column",
-    backgroundColor: "#fff"
-  },
   container: {
     flex: 1,
-    flexDirection: "column",
-    padding: 5,
-    backgroundColor: "#fff",
-    flexWrap: "wrap"
+    backgroundColor: "#ababab"
+  },
+  player: {
+    position: "absolute",
+    backgroundColor: "pink",
+    width: RADIUS * 2,
+    height: RADIUS * 2,
+    borderRadius: RADIUS * 2
+  },
+  square: {
+    position: "absolute",
+    backgroundColor: "#17BEBB",
+    width: SQUARE * 2,
+    height: SQUARE * 2
   }
 });
 
-function mapStateToProps(state, props) {
-  return {
-    ...state
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators(Actions, dispatch);
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(CasinoPage);
+AppRegistry.registerComponent("BestGameEver", () => BestGameEver);
