c9f04fc67130c40b2bda186c3097216474c60590
kyle brown <kylebrown@kyles-MacBook-Pro.local>
2018-05-22 12:49:17 -0700

    created a market component. farmers market displaying items.

---
+++ components/store/MarketSquare.js
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Animated,
+  View,
+  TouchableOpacity,
+  Text,
+  Image
+} from "react-native";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import * as Actions from "../../actions/actions";
+
+class MarketSquare extends Component {
+  state = {
+    flipIn: new Animated.Value(0)
+  };
+  flipInFun(flip) {
+    Animated.timing(flip, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true
+    }).start();
+  }
+  flipOutFun(flip) {
+    Animated.timing(flip, {
+      toValue: 0,
+      duration: 500,
+      useNativeDriver: true
+    }).start();
+  }
+  render() {
+    const { image, name } = this.props.item;
+    return (
+      <View
+        style={[
+          {
+            backfaceVisibility: "hidden",
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            minWidth: 100
+          },
+          { minHeight: 144 }
+        ]}
+      >
+        <TouchableOpacity
+          onPress={() => {
+            // this.props.popUpButton();
+          }}
+          style={[
+            {
+              backfaceVisibility: "hidden",
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "#DEDBD2",
+              minWidth: 100,
+              backgroundColor: "#FF5714",
+              position: "absolute"
+            }
+          ]}
+        >
+          <Image
+            resizeMode="contain"
+            style={[styles.images, { margin: 5 }]}
+            source={{ uri: image }}
+          />
+          <Text>{name}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  nameText: { fontSize: 14, fontWeight: "100" },
+  images: { height: 100, width: 100 },
+  itemTouch: {
+    backfaceVisibility: "hidden",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#DEDBD2",
+    minWidth: 100,
+    backgroundColor: "#C2847A",
+    position: "absolute"
+  },
+  imagesBack: { height: 50, width: 50 },
+  singleBox: {
+    backfaceVisibility: "hidden",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#DEDBD2",
+    minWidth: 100
+  }
+});
+
+function mapStateToProps(state, props) {
+  return {
+    ...state
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(Actions, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MarketSquare);
--- pages/FarmersMarketPage.js
+++ pages/FarmersMarketPage.js
@@ -1,82 +1,121 @@
 import React, { Component } from "react";
-import { Animated, StyleSheet, Text, View } from "react-native";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import {
+  Animated,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  TouchableOpacity
+} from "react-native";
 import SquareRow from "../components/square/SquareRow";
 import TimerMixin from "react-timer-mixin";
 import MapPage from "./MapPage";
+import _ from "lodash";
+import * as Actions from "../actions/actions";
+import TitleHeader from "./helperFunctions/TitleHeader";
+import MarketSquare from "../components/store/MarketSquare";
 
-export default class FarmersMarketPage extends Component {
-  state = {
-    fadeAnim: new Animated.Value(0),
-    mixins: [TimerMixin]
-  };
-
-  componentDidMount() {
-    Animated.timing(this.state.fadeAnim, {
-      toValue: 1,
-      duration: 3000,
-      useNativeDriver: true
-    }).start();
-
-    setTimeout(() => {
-      this.props.navigator.push({
-        component: MapPage,
-        title: "Map Page",
-        passProps: {
-          navigator: this.props.navigator
-        }
-      });
-    }, 5000);
-  }
-
+class FarmersMarket extends Component {
   render() {
-    const { color } = this.props;
-    const { fadeAnim } = this.state;
-    const wholeColor =
-      color.length === 5
-        ? color + "0"
-        : color.length === 4
-          ? color + "00"
-          : color;
     return (
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Text style={[styles.headerText, { flex: 0.5 }]}>YOU GOT A</Text>
-          <Text style={[styles.headerText, { flex: 0.3 }]}>{wholeColor}</Text>
-        </View>
-        <Animated.View
-          style={[
-            {
-              flex: 1,
-              backgroundColor: `#${wholeColor}`
-            },
-            {
-              opacity: fadeAnim
-            }
-          ]}
+      <ScrollView style={[styles.container]}>
+        <TitleHeader
+          title={"Farmers Market"}
+          callback={() => this.props.navigator.pop()}
         />
-      </View>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            alignContent: "center",
+            alignItems: "center"
+          }}
+        >
+          <View style={styles.storeContents}>
+            {_.map(
+              [
+                {
+                  name: "dart",
+                  price: "100",
+                  description: "Increases a single pet's accuracy by 5",
+                  image: "/Users/kylebrown/futurePets/images/market/dart.png"
+                },
+                {
+                  name: "ticket",
+                  price: "1000",
+                  description: "A ticket to the next town!",
+                  image: "/Users/kylebrown/futurePets/images/market/ticket.png"
+                }
+              ],
+              (item, index) => {
+                return <MarketSquare item={item} key={index} />;
+              }
+            )}
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  images: { width: 35, height: 35 },
+  mainText: {
+    flex: 7,
+    flexDirection: "column",
+    alignContent: "center"
+  },
   headerText: {
+    flex: 5,
     fontSize: 40,
-    textAlign: "center"
+    textAlign: "center",
+    position: "absolute"
+  },
+  storeContents: {
+    flexWrap: "wrap",
+    // marginTop: 10,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 5
   },
   header: {
     flex: 0.3,
     marginTop: 20,
     flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
     backgroundColor: "#fff"
+  },
+  sections: {
+    marginTop: 20,
+    padding: 5,
+    height: 50,
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignContent: "flex-start",
+    borderColor: "#CBBFBB",
+    borderRadius: 5,
+    borderWidth: 1
   },
   container: {
     flex: 1,
     flexDirection: "column",
     padding: 5,
-    backgroundColor: "#F5FCFF",
+    backgroundColor: "#fff",
     flexWrap: "wrap"
   }
 });
+
+function mapStateToProps(state, props) {
+  return {
+    ...state
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(Actions, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FarmersMarket);
